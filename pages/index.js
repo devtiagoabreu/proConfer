6a0d533c7cbcee43cebeaec24f472bd1b5a97bb2
Tiago Abreu <devtiagoabreu@gmail.com>
2022-08-12 14:59:17 -0300

    implementando um contador para teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
+import {useState } from 'react';
+
 function Home() {
-    return <div>Home</div>
+    return (
+        <div>
+            <h1>Home</h1>
+            <Contador />
+        </div>
+    ) 
+}
+
+function Contador() {
+    const [contador,setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+    return (
+        <div>
+            <div>
+                {contador}
+                <button onClick={adicionarContador}>Adicionar</button>
+            </div>
+        </div>
+    )
 }
 
 export default Home
