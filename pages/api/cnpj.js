4919474ab58eca01f947fefdbec086c16d5baf71
Tiago Abreu <devtiagoabreu@gmail.com>
2022-08-16 20:50:55 -0300

    corrigindo url api

--- pages/api/cnpj.js
+++ pages/api/cnpj.js
@@ -2,7 +2,7 @@ async function cnpj(request, response) {
     const api_cnpj = process.env.API_CNPJ;
     const dynamicDate = new Date();
 
-    const dadosCnpj = await fetch(`https://receitaws.com.br/account/api/${api_cnpj}`);
+    const dadosCnpj = await fetch(`https://receitaws.com.br/v1/cnpj/${api_cnpj}`);
     const dadosCnpjJson = await dadosCnpj.json();
     const razaoSocial = dadosCnpjJson.nome;
 
