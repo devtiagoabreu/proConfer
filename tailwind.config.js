b5d8ef80bb0ffeb1a94a8d410cffa38f4c7af20e
Tiago Abreu <devtiagoabreu@gmail.com>
2022-08-18 15:27:00 -0300

    tailwindcss | uuid

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}', 
+    './src/components/**/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
